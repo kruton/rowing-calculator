94d6ea0b40cbce41767a7067d6b2e73fdf9a83e0
Kenny Root <kenny@the-b.org>
2024-12-02 19:45:00 -0800

    Switch to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link, useNavigate, useLocation } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Link, useNavigate, useLocation } from 'react-router-dom'
 import {
   ChakraProvider,
   Box,
