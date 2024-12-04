c28cdafa01da0c17dc4f64d9a2153ae82e7ad837
Kenny Root <kenny@the-b.org>
2024-12-03 22:26:27 -0800

    Add back color toggle

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import {
   ChakraProvider,
   Box,
   Container,
+  IconButton,
   VStack,
   Flex,
   Text,
+  useColorMode,
 } from '@chakra-ui/react'
+import { MoonIcon, SunIcon } from '@chakra-ui/icons'
 import { keyframes } from '@emotion/react'
 import { extendTheme } from '@chakra-ui/react'
 import TimeCalculator from './components/TimeCalculator'
@@ -43,31 +46,39 @@ const theme = extendTheme({
 })
 
 function AppContent() {
+  const { colorMode, toggleColorMode } = useColorMode()
+
   return (
     <Flex width={"100vw"} height={"100vh"} alignContent={"center"} justifyContent={"center"}>
       <Box minH="100vh" py={[4, 8]} px={[2, 4]}>
         <Container maxW="container.md">
-          <VStack spacing={[4, 8]} align="stretch">
-            <Flex align="center" justify="space-between" mb={-2}>
-              <Flex flex="1" justify="center" align="center">
-                <Text
-                  as="span"
-                  fontSize={["xl", "2xl"]}
-                  fontFamily="logo"
-                  fontWeight="900"
-                  letterSpacing="wider"
-                  textTransform="uppercase"
-                  bgSize="300% 300%"
-                  bgGradient="linear(to-r, blue.400, cyan.400, purple.400, pink.400, blue.400)"
-                  bgClip="text"
-                  css={{
-                    animation: `${gradientAnimation} 8s ease infinite`,
-                  }}
-                  style={{ textShadow: '0 0 20px rgba(0,149,255,0.15)' }}
-                >
-                  Rowing Calculator
-                </Text>
-              </Flex>
+          <VStack spacing={[0, 0]} align="stretch">
+            <Flex flex="1" justify="center" align="center" mr={-2}>
+              <Text
+                as="span"
+                fontSize={["xl", "2xl"]}
+                fontFamily="logo"
+                fontWeight="900"
+                letterSpacing="wider"
+                textTransform="uppercase"
+                bgSize="300% 300%"
+                bgGradient="linear(to-r, blue.400, cyan.400, purple.400, pink.400, blue.400)"
+                bgClip="text"
+                css={{
+                  animation: `${gradientAnimation} 8s ease infinite`,
+                }}
+                style={{ textShadow: '0 0 20px rgba(0,149,255,0.15)' }}
+              >
+                Rowing Calculator
+              </Text>
+              <IconButton ml={5}
+                aria-label="Toggle color mode"
+                icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+                onClick={toggleColorMode}
+                colorScheme={colorMode === 'light' ? 'purple' : 'yellow'}
+                variant="outline"
+                size="md"
+              />
             </Flex>
 
             <Box
