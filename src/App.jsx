fa66435c0a70be14343f9f29395458d0f55525bd
Kenny Root <kenny@the-b.org>
2025-02-23 10:01:04 -0800

    Pull out website title to its own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { Provider } from "@/components/ui/provider";
 import TimeCalculator from "@/components/TimeCalculator";
+import { WebsiteTitle } from "./components/WebsiteTitle";
 import { ColorModeButton, useColorMode } from "@/components/ui/color-mode";
-import { Box, Container, Flex, Text, VStack } from "@chakra-ui/react";
+import { Box, Container, Flex, VStack } from "@chakra-ui/react";
 
 function AppContent() {
   const { colorMode } = useColorMode();
@@ -11,24 +12,7 @@ function AppContent() {
         <Container maxW="container.md">
           <VStack spacing={[4, 4]} align="stretch">
             <Flex flex="1" justify="center" align="center">
-              <Text
-                as="span"
-                fontSize={["xl", "2xl"]}
-                fontFamily="logo"
-                fontWeight="900"
-                letterSpacing="wider"
-                textTransform="uppercase"
-                bgSize="300% 300%"
-                bgGradient="to-r"
-                gradientFrom={"blue.400"}
-                gradientVia={["cyan.400", "purple.400", "pink.400"]}
-                gradientTo={"blue.400"}
-                bgClip="text"
-                animation="gradientAnimation"
-                style={{ textShadow: "0 0 20px rgba(0,149,255,0.15)" }}
-              >
-                Rowing Calculator
-              </Text>
+              <WebsiteTitle />
               <ColorModeButton mx={2} />
             </Flex>
 
